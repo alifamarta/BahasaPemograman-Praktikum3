8380f8cbde82296f25318e99cb6b02d030cf439e
Alif Amarta <alifamarta23@gmail.com>
2022-10-26 00:50:35 +0700

    Add files via upload

--- Lingkaran.py
+++ Lingkaran.py
@@ -1,7 +1,7 @@
 pi = 3.14
-jari = float(input("Masukkan Jari-Jari: "))
-luas = pi*(jari * jari)
-keliling = 2 * pi * jari
+r = float(input("Masukkan Jari-Jari: "))
+luas = pi* r ** 2
+keliling = 2 * pi * r
 
 print("Luas Lingkaran =","{:.1f}".format(luas))
 print("Keliling Lingkaran =","{:.1f}".format(keliling))
