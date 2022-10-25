fb92298a5eccfb63a11518835c003f67347e1d36
Alif Amarta <alifamarta23@gmail.com>
2022-10-25 09:42:51 +0700

    Add files via upload

---
+++ Praktikum3.py
@@ -0,0 +1,7 @@
+pi = 3.14
+jari = float(input("Masukkan Jari-Jari: "))
+luas = pi*(jari * jari)
+keliling = 2 * pi * jari
+
+print("Luas Lingkaran =","{:.1f}".format(luas))
+print("Keliling Lingkaran =","{:.1f}".format(keliling))
